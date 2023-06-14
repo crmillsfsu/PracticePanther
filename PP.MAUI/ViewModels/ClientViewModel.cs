3f4d5f69e546ac4ed4e3aa32ef68206563d8701c
crmills <crmills@fsu.edu>
2023-06-14 10:15:22 -0400

    Implemented Delete for clients Closes #4

--- PP.MAUI/ViewModels/ClientViewModel.cs
+++ PP.MAUI/ViewModels/ClientViewModel.cs
@@ -3,20 +3,41 @@ using PP.Library.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace PP.MAUI.ViewModels
 {
-    public class ClientViewModel
+    public class ClientViewModel : INotifyPropertyChanged
     {
+        public Client SelectedClient { get; set; }
 
         public ObservableCollection<Client> Clients { 
             get
             {
                 return new ObservableCollection<Client>(ClientService.Current.Clients);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Delete()
+        {
+            if(SelectedClient != null)
+            {
+                ClientService.Current.Delete(SelectedClient.Id);
+                SelectedClient= null;
+                NotifyPropertyChanged(nameof(Clients));
+                NotifyPropertyChanged(nameof(SelectedClient));
             }
         }
 
